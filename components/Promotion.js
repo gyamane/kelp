5fa7b9c3e08e2f48a42b4253285d0da770df0490
kalvinfromcode <kalvin1204@gmail.com>
2019-01-14 22:56:49 -0500

    home screen progress

---
+++ components/Promotion.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export class Promotion extends React.Component {
+  
+}
---
+++ components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export class Search extends React.Component {
+
+}
